bd9f755e4e03f540faaab01e5511be118c42978a
vijayda11458 <81608124+vijayda11458@users.noreply.github.com>
2021-06-18 21:16:46 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -61,19 +61,22 @@ def post(id):
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
+        app.logger.info('already logged in successfully')
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            app.logger.info('login failed: Invalid username or password, {}'.format(form.username.data))
-            flash('Invalid username or password')
+            flash('Invalid User Name/Password Please Retype')
+            app.logger.info('Invalid Username or Password')
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
-        app.logger.info('login successful: User logged in, {}'.format(form.username.data))
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
+
+        app.logger.info('logged in successfully')
+		
         return redirect(next_page)
     session["state"] = str(uuid.uuid4())
     auth_url = _build_auth_url(scopes=Config.SCOPE, state=session["state"])
