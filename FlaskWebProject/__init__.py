5a16eeb8bb0ff3de7f0b71d835b04737c31d05b4
vijayda11458 <81608124+vijayda11458@users.noreply.github.com>
2021-06-22 11:13:49 +0530

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -11,6 +11,11 @@ from flask_session import Session
 app = Flask(__name__)
 app.config.from_object(Config)
 # TODO: Add any logging levels and handlers with app.logger
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+app.logger.info('Server Started')
 Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
