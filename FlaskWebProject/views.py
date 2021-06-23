d3d4a2fdec92ff3fb4cedb32a1604939a0171251
vijayda11458 <81608124+vijayda11458@users.noreply.github.com>
2021-06-23 13:14:29 +0530

    Update views.py

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -71,6 +71,7 @@ def login():
             return redirect(url_for('login'))
         login_user(user, remember=form.remember_me.data)
         app.logger.info('Successful User Login')
+        flash('Successful User Login')
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
             next_page = url_for('home')
